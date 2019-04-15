9ace1610d0f84118a7a6a866ec847d3dd4c1e7ba
caorui <caorui@boe.com.cn>
2019-04-15 17:06:26 +0800

    增加数据层接口

---
+++ Helios/DAO/IDBHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helios.DAO
+{
+    public interface IDBHelper
+    {
+        /// <summary>
+        /// 打开数据库连接并开启事务
+        /// </summary>
+        void BeginTransaction();
+
+        /// <summary>
+        /// 提交事务并关闭数据库连接
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// 回滚事务并关闭数据库连接
+        /// </summary>
+        void RollbackTransaction();
+
+        /// <summary>
+        /// 在已开启的事务中执行增删改操作，返回受影响的行数
+        /// </summary>
+        /// <param name="sql">执行语句</param>
+        /// <param name="parameters">执行语句参数</param>
+        /// <returns></returns>
+        int ExecuteNonQueryWithTransaction(string sql, object[] parameters);
+
+        /// <summary>
+        /// 执行增删改操作，返回受影响的行数 
+        /// </summary>
+        /// <param name="sql">执行语句</param>
+        /// <param name="parameters">执行语句参数</param>
+        /// <returns></returns>
+        int ExecuteNonQuery();
+
+        /// <summary>
+        /// 批量处理增删改操作语句
+        /// </summary>
+        /// <param name="list">执行语句与参数集合</param>
+        void ExecuteNonQueryBatch(List<KeyValuePair<string, object[]>> list);
+
+        /// <summary>
+        /// 执行整表插入数据操作
+        /// </summary>
+        /// <param name="tableName">需要操作的表名</param>
+        /// <param name="dt">需要插入的表数据</param>
+        /// <returns></returns>
+        int ExecuteInsertByDataTable(string tableName, DataTable dt);
+
+        /// <summary>
+        /// 查询并返回不含数据的表结构
+        /// </summary>
+        /// <param name="tableName">需要查询的表名</param>
+        /// <returns></returns>
+        DataTable ExecuteQueryTableStructure(string tableName);
+
+        /// <summary>
+        /// 执行查询语句，并返回查询结果的DataTable实例
+        /// </summary>
+        /// <param name="sql">查询语句</param>
+        /// <param name="parameters">查询语句参数</param>
+        /// <returns></returns>
+        DataTable ExecuteQuery(string sql, object[] parameters);
+
+        /// <summary>
+        /// 执行查询语句，并返回第一行第一列的数据
+        /// </summary>
+        /// <param name="sql">查询语句</param>
+        /// <param name="parameters">查询语句参数</param>
+        /// <returns></returns>
+        object ExecuteScalar(string sql, object[] parameters);
+
+        /// <summary>
+        /// 执行查询语句，并返回查询结果的泛型实例序列
+        /// </summary>
+        /// <typeparam name="T">指定的泛型</typeparam>
+        /// <param name="sql">查询语句</param>
+        /// <param name="parameters">查询语句参数</param>
+        /// <returns></returns>
+        List<T> ExecuteQueryForList<T>(string sql, object[] parameters);
+
+        /// <summary>
+        /// 执行查询语句，并返回查询结果的泛型实例集合
+        /// </summary>
+        /// <typeparam name="T">指定的泛型</typeparam>
+        /// <param name="sql">查询语句</param>
+        /// <param name="parameters">查询语句参数</param>
+        /// <returns></returns>
+        Collection<T> ExecuteQueryForCollection<T>(string sql, object[] parameters);
+    }
+}
---
+++ Helios/DAO/MySqlHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Helios.DAO
+{
+    class MySqlHelper : IDBHelper
+    {
+        private static string server = System.Configuration.ConfigurationManager.AppSettings["DatabaseServer"];
+        private static string dbName = System.Configuration.ConfigurationManager.AppSettings["DatabaseName"];
+        private static string dbUser = System.Configuration.ConfigurationManager.AppSettings["DatabaseUser"];
+        private static string dbPassword = System.Configuration.ConfigurationManager.AppSettings["DatabasePassword"];
+        private static string dbCharset = System.Configuration.ConfigurationManager.AppSettings["DatabaseCharset"];
+        private static string dbPooling = System.Configuration.ConfigurationManager.AppSettings["DatabasePooling"];
+        public static string connectionString = string.Format("Data Source={0};Database={1};User Id={2};Password={3};charset={4};pooling={5}", server, dbName, dbUser, dbPassword, dbCharset, dbPooling);
+
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommitTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteInsertByDataTable(string tableName, DataTable dt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteNonQueryBatch(List<KeyValuePair<string, object[]>> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteNonQueryWithTransaction(string sql, object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable ExecuteQuery(string sql, object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Collection<T> ExecuteQueryForCollection<T>(string sql, object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> ExecuteQueryForList<T>(string sql, object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable ExecuteQueryTableStructure(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ExecuteScalar(string sql, object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollbackTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
